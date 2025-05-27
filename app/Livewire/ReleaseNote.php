251149e1315bec10df2d4853994b8682527ee9fd
Boolean-true <i22014@hb.dhbw-stuttgart.de>
2025-05-27 12:16:25 +0200

    docs: add release notes

--- app/Livewire/ReleaseNote.php
+++ app/Livewire/ReleaseNote.php
@@ -4,4 +4,22 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
-class ReleaseNote extends Component {}
+class ReleaseNote extends Component
+{
+    public $notes = [];
+
+    public function mount()
+    {
+        $this->notes = [
+            'Neue Features' => [
+                'User Management',
+                'Automatische Extrahierung der Bestandteile Anrede, Titel, Geschlecht, Vorname, Nachname und Sprache aus der Eingabe',
+                'Manuelle Anpassung der Bestandteile, bei der auch die Briefanrede angepasst wird',
+                'Hinzufügen neuer Titel',
+                'Automatische Generierung einer Briefanrede',
+                'Speichern und Abrufen von Briefanreden',
+                'Zentrale Verwaltung der Konfiguration',
+            ],
+        ];
+    }
+}
--- resources/views/livewire/release-note.blade.php
+++ resources/views/livewire/release-note.blade.php
@@ -1,3 +1,19 @@
 <div>
     <flux:heading size="xl">Release Note</flux:heading>
+
+    <div class="max-w-3xl px-4">
+        @foreach ($notes as $title => $noteList)
+            <div class="max-w-3xl px-4">
+                <flux:heading class="mt-3" size="lg">
+                    {{ $title }} </flux:heading>
+                <ol class="list-disc pl-6 space-y-2 mt-1">
+                    @foreach ($noteList as $note)
+                        <li>
+                            <flux:text>{{ $note }}</flux:text>
+                        </li>
+                    @endforeach
+                </ol>
+            </div>
+        @endforeach
+    </div>
 </div>
