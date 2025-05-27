344caa4e15c7d803c47320fcf7676846b306cdc5
Boolean-true <i22014@hb.dhbw-stuttgart.de>
2025-05-28 00:23:14 +0200

    docs: add user manual to release notes

--- resources/views/livewire/release-note.blade.php
+++ resources/views/livewire/release-note.blade.php
@@ -16,6 +16,15 @@
             </div>
         @endforeach
         <flux:heading class="mt-3" size="lg">
+            Anleitung
+        </flux:heading>
+        <flux:text class="mt-1">
+            Nach der Anmeldung wird die Splitter-Seite gezeigt. Dort kann die unstrukturierte Eingabe in das Textfeld eingegeben werden. Bei jeder Änderung wird die Extraktion neu gestartet. Die Felder können manuell angepasst werden, wodurch die Briefanrede automatisch angepasst wird. Bei Bedarf kann diese Eingabe mitsamt Briefanrede abgespeichert werden. Diese kann in der Seitennavigation unter "Anreden" eingesehen werden. Dort kann diese auch gelöscht werden. 
+        </flux:text>
+        <flux:text class="mt-1">
+            Bei "Titel" kann ein neuer Titel hinzugefügt werden, der noch nicht in den Standardtiteln enthalten sein darf. Die angelegten Titel werden mitsamt der Standardtiteln bei der Titelextraktion berücksichtigt.
+        </flux:text>
+        <flux:heading class="mt-3" size="lg">
             Tests
         </flux:heading>
         <flux:text class="mt-1">
